09f631119f8f84c8c9df681486a007e9f114828f
emrahcimren <ecimren@starbucks.com>
2020-03-24 17:06:46 -0700

    update data added kmedian.py added kmedian_inputs.py added kmedian_models.py

--- kmedian_geo/str/data.py
+++ kmedian_geo/str/data.py
@@ -2,6 +2,6 @@ import pandas as pd
 import os
 current_dir, this_filename = os.path.split(__file__)
 
-stores = pd.read_pickle(os.path.join(current_dir, '../data/stores.pkl'))
-facilities = pd.read_pickle(os.path.join(current_dir, '../data/facilities.pkl'))
-costs = pd.read_pickle(os.path.join(current_dir, '../data/costs.pkl'))
+stores = pd.read_pickle(os.path.join(current_dir, 'kmedian_geo/data/stores.pkl'))
+facilities = pd.read_pickle(os.path.join(current_dir, 'kmedian_geo/data/facilities.pkl'))
+costs = pd.read_pickle(os.path.join(current_dir, 'kmedian_geo/data/costs.pkl'))
